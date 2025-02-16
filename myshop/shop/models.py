daa57ce75fb68cf7675cb87fff70b3a01ee6f778
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-02-16 22:20:33 +0100

    Create Category and Product classes

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Category class """
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        """ Metadata """
+        ordering = ['name']
+        indexes = [models.Index(fields=['name']), ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        """ String representation """
+        return self.name
+
+
+class Product(models.Model):
+    """ Product class """
+    category = models.ForeignKey(Category, related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """ metadata """
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        """ string representation """
+        return self.name
