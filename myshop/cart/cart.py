7c375311c1eab2993c4e8b534298a0cf83b36af6
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-02-22 08:31:08 +0100

    Modified Cart model to correct a typo error

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -52,7 +52,7 @@ class Cart:
         product_ids = self.cart.keys()
 
         # get the product objects and add them to the cart
-        products = Product.object.filter(id__in=product_ids)
+        products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
 
         for product in products:
