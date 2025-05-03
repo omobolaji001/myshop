9e5f247bd69bf2b2e0e3d85816a63885e7b4bb6f
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-05-03 16:45:06 +0100

    Modified the Order Admin to implement the pdf view

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,8 +1,10 @@
-import csv
+import cs
 import datetime
 from django.http import HttpResponse
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -33,6 +35,29 @@ def export_to_csv(modeladmin, request, queryset):
 
 export_to_csv.short_description = 'Export to CSV'
 
+def order_payment(obj):
+    """ Returns the link to Stripe_id """
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+order_payment.short_description = 'Stripe payment'
+
+def order_detail(obj):
+    """ Displays the details of an order """
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+
+    return mark_safe(f'<a href="{url}">View</a>')
+
+def order_pdf(obj):
+    """ Displays the order pdf """
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+order_pdf.short_description = 'Invoice'
 
 class OrderItemInline(admin.TabularInline):
     """ Allows to includes OrderItem 
@@ -45,8 +70,11 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     """ Register Order model to the admin page """
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-                    'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = [
+        'id', 'first_name', 'last_name', 'email', 'address',
+        'postal_code', 'city', 'paid', order_payment,
+        'created', 'updated', order_detail, order_pdf,
+    ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
