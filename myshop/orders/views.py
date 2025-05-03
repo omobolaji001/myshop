954e14d926b7fc4a3ce4353a88aa18ade27aa383
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-05-03 16:41:03 +0100

    Implemented additional views for the Admin site

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render, redirect
+import weasyprint
+from django.contrib.staticfiles import finders
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import Order, OrderItem
 from .tasks import order_created
 from cart.cart import Cart
 
@@ -39,3 +44,24 @@ def order_create(request):
             request, 'orders/order/create.html',
             {'cart': cart, 'form': form}
         )
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    """ Displays information about an order """
+    order = get_object_or_404(Order, id=order_id)
+
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    """ Generates a pdf file for an order """
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[weasyprint.CSS(finders.find('css/pdf.css'))]
+    )
+
+    return response
