0ec5468ee3278cce14c4093f6921e08feb81f22b
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-05-17 11:14:02 +0100

    Modified Cart to accomodate coupons for the shopping cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart:
@@ -14,6 +15,9 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
 
         self.cart = cart
+
+        # store current applied coupon
+        self.coupon_id = self.session.get('coupon_id')
 
     def add(self, product, quantity=1, override_quantity=False):
         """ Add a product to the cart or update its quantity """
@@ -71,6 +75,28 @@ class Cart:
         """ Calculate the total price of items in the cart """
         return sum(Decimal(item['price']) * item['quantity']
                    for item in self.cart.values())
+
+    @property
+    def coupon(self):
+        """ Returns the coupon ID """
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+
+        return None
+
+    def get_discount(self):
+        """ Returns the discount based on the coupon applied """
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        """ Returns the discounted price """
+        return self.get_total_price() - self.get_discount()
 
     def clear(self):
         """ Remove cart from session """
