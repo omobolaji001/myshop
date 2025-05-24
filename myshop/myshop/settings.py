a075386f362fe0429a9ff030aaf5f7f9552c73bb
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-05-24 21:39:20 +0100

    Modified settings.py to register the Coupons app

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
+    'coupons.apps.CouponsConfig',
     'shop.apps.ShopConfig',
 ]
 
