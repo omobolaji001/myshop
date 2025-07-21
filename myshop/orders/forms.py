4c29d781a2f859a88d760c9c0d3d26cbf8ab14a7
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-07-21 15:28:26 +0100

    validate postal code with localflavor

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from .models import Order
+from localflavor.us.forms import USZipCodeField
 
 
 class OrderCreateForm(forms.ModelForm):
     """ Allows user to create an order """
+    postal_code = USZipCodeField()
+
     class Meta:
         """ metadata """
         model = Order
-        fields = ['first_name', 'last_name', 'email',
-                  'address', 'postal_code', 'city']
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'address',
+            'postal_code',
+            'city'
+        ]
