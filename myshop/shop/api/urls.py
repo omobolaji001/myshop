46738ce519136b0a6cda7a895fe609682736ebc6
omobolaji001 <omobolajiolayiwola11@gmail.com>
2025-10-04 15:41:43 +0100

    Modified the shop app

--- myshop/shop/api/urls.py
+++ myshop/shop/api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from rest_framework import routers
 from shop.api import views
 
+app_name = 'shop'
 
 router = routers.DefaultRouter()
-router.register('categories', views.CategoryViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'products', views.ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- myshop/shop/api/views.py
+++ myshop/shop/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from shop.models import Category
-from shop.api.serializers import CategorySerializer
+from shop.models import Category, Product
+from shop.api.serializers import CategorySerializer, ProductSerializer
 from shop.api.pagination import StandardPagination
 
 
@@ -10,4 +10,13 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Category.objects.prefetch_related('products')
     serializer_class = CategorySerializer
+    pagination_class = StandardPagination
+
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    """ Provides read-only action to list Product objects
+    or retrieve a Product object.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
     pagination_class = StandardPagination
